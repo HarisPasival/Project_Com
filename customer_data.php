fa4faf06c7c26edce56063e887222e4164c92a68
HarisPasival <624234018@parichat.skru.ac.th>
2022-09-08 00:04:31 +0700

    8/9/65

--- customer_data.php
+++ customer_data.php
@@ -9,7 +9,6 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- <script src="https://kit.fontawesome.com/79a0376aeb.js" crossorigin="anonymous"></script>  -->
     <title>ข้อมูลลูกค้า</title>
     <?php include 'navbar/head.php'?>
 </head>
